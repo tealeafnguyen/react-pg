30ef8edc8a08bd56480bc748a2348a884f2f1660
Thomas Nguyen <tealeafnguyen@gmail.com>
2018-12-17 15:22:42 -0800

    Updates

---
+++ precache-manifest.52a024afbf0f5070e71825253c0c30af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a436b19b9f3b4ee09f08",
+    "url": "/react-pg/static/js/runtime~main.a436b19b.js"
+  },
+  {
+    "revision": "b3ce4174e1b10767051f",
+    "url": "/react-pg/static/js/main.b3ce4174.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/react-pg/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "b3ce4174e1b10767051f",
+    "url": "/react-pg/static/css/main.a8d0c5de.chunk.css"
+  },
+  {
+    "revision": "7c58801f19071073e755e57e107fe36e",
+    "url": "/react-pg/index.html"
+  }
+];
